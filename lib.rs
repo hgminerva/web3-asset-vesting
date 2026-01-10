2f9540d8ab19a8be7cd7317c35d3916ac4e784bd
hgminerva <hgminerva@gmail.com>
2026-01-11 06:56:22 +0800

    Added add_vested_balance Method

--- lib.rs
+++ lib.rs
@@ -3,6 +3,38 @@
 #[ink::contract]
 mod vesting {
 
+    /// Error Messages
+    #[derive(scale::Encode, scale::Decode, Debug, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// There is already an existing vested balance for that address
+        VestedBalanceExist,
+    }
+
+    /// Success Messages
+    #[derive(scale::Encode, scale::Decode, Debug, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Success {
+        /// Success adding vested balance
+        VestedBalanceAdded,
+    }
+
+    /// Vesting Status
+    #[derive(scale::Encode, scale::Decode, Debug, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum VestingStatus {
+        EmitSuccess(Success),
+        EmitError(Error),
+    }
+
+    /// Vesting Event
+    #[ink(event)]
+    pub struct VestingEvent {
+        #[ink(topic)]
+        operator: AccountId,
+        status: VestingStatus,
+    } 
+
     /// Vested balances
     #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
@@ -41,42 +73,59 @@ mod vesting {
         /// Vested balances
         pub vested_balances: Vec<VestedBalance>,
         /// Requested balances
-        pub vested_balances: Vec<VestedBalance>,
+        pub requested_balances: Vec<RequestedBalance>,
     }
 
-    impl Web3AssetVesting {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    impl Vesting {
+        /// Constructor 
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(asset_id: u128) -> Self {
+
+            Self { 
+                asset_id: asset_id, 
+                vested_balances: Vec::new(),
+                requested_balances: Vec::new(),
+            }
+
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
+        /// Default
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+
+            Self::new(0u128)
+
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// Add a vested balance
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
+        pub fn add_vested_balance(&mut self,
+            address: AccountId,
+            original_balance: u128,) -> Result<(), Error> {
+            
+            let caller = self.env().caller();
 
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+            /// The initial vested balance are frozen
+            let new_vested_balance = VestedBalance {
+                address: address,
+                original_balance: original_balance,
+                free_balance: 0,
+                frozen_balance: original_balance,
+                total_requested_balance: 0,
+            };
+
+            self.vested_balances.push(new_vested_balance);
+
+            self.env().emit_event(VestingEvent {
+                operator: caller,
+                status: VestingStatus::EmitSuccess(Success::VestedBalanceAdded),
+            });
+
+            Ok(())
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
+    /// Unit tests
     #[cfg(test)]
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
@@ -85,26 +134,14 @@ mod vesting {
         /// We test if the default constructor does its job.
         #[ink::test]
         fn default_works() {
-            let web3_asset_vesting = Web3AssetVesting::default();
-            assert_eq!(web3_asset_vesting.get(), false);
+            let vesting = Vesting::default();
+            assert_eq!(vesting.get(), false);
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut web3_asset_vesting = Web3AssetVesting::new(false);
-            assert_eq!(web3_asset_vesting.get(), false);
-            web3_asset_vesting.flip();
-            assert_eq!(web3_asset_vesting.get(), true);
-        }
     }
 
 
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
+    /// End-to-end (E2E) or integration tests for ink! contracts.
     #[cfg(all(test, feature = "e2e-tests"))]
     mod e2e_tests {
         /// Imports all the definitions from the outer scope so we can use them here.
@@ -120,17 +157,17 @@ mod vesting {
         #[ink_e2e::test]
         async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
             // Given
-            let constructor = Web3AssetVestingRef::default();
+            let constructor = VestingRef::default();
 
             // When
             let contract_account_id = client
-                .instantiate("web3_asset_vesting", &ink_e2e::alice(), constructor, 0, None)
+                .instantiate("vesting", &ink_e2e::alice(), constructor, 0, None)
                 .await
                 .expect("instantiate failed")
                 .account_id;
 
             // Then
-            let get = build_message::<Web3AssetVestingRef>(contract_account_id.clone())
+            let get = build_message::<VestingRef>(contract_account_id.clone())
                 .call(|web3_asset_vesting| web3_asset_vesting.get());
             let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
             assert!(matches!(get_result.return_value(), false));
@@ -142,20 +179,20 @@ mod vesting {
         #[ink_e2e::test]
         async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
             // Given
-            let constructor = Web3AssetVestingRef::new(false);
+            let constructor = VestingRef::new(false);
             let contract_account_id = client
-                .instantiate("web3_asset_vesting", &ink_e2e::bob(), constructor, 0, None)
+                .instantiate("vesting", &ink_e2e::bob(), constructor, 0, None)
                 .await
                 .expect("instantiate failed")
                 .account_id;
 
-            let get = build_message::<Web3AssetVestingRef>(contract_account_id.clone())
+            let get = build_message::<VestingRef>(contract_account_id.clone())
                 .call(|web3_asset_vesting| web3_asset_vesting.get());
             let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
             assert!(matches!(get_result.return_value(), false));
 
             // When
-            let flip = build_message::<Web3AssetVestingRef>(contract_account_id.clone())
+            let flip = build_message::<VestingRef>(contract_account_id.clone())
                 .call(|web3_asset_vesting| web3_asset_vesting.flip());
             let _flip_result = client
                 .call(&ink_e2e::bob(), flip, 0, None)
@@ -163,7 +200,7 @@ mod vesting {
                 .expect("flip failed");
 
             // Then
-            let get = build_message::<Web3AssetVestingRef>(contract_account_id.clone())
+            let get = build_message::<VestingRef>(contract_account_id.clone())
                 .call(|web3_asset_vesting| web3_asset_vesting.get());
             let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
             assert!(matches!(get_result.return_value(), true));
