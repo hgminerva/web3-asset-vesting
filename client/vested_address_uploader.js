adaf1808a0257bb6bcf4165a84f4506eb2af5dc6
hgminerva <hgminerva@gmail.com>
2026-01-29 09:21:25 +0800

    GEM Vesting1 (Sample try)

---
+++ client/vested_address_uploader.js
@@ -0,0 +1,78 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ContractPromise } from "@polkadot/api-contract";
+import { Keyring } from "@polkadot/keyring";
+import fs from "fs";
+import 'dotenv/config';
+import { decode } from "./decode.js";
+import csv from "csv-parser";
+
+/// Blockchain
+const WS_ENDPOINT = process.env.WS_ENDPOINT;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_ABI_PATH = process.env.CONTRACT_ABI_PATH;
+const ALICE = process.env.ALICE;
+
+console.log("Connecting to blockchain...");
+const wsProvider = new WsProvider(WS_ENDPOINT);
+const api = await ApiPromise.create({ provider: wsProvider });
+console.log("Connected to:", (await api.rpc.system.chain()).toHuman());
+
+/// Contract
+const abiJSON = JSON.parse(fs.readFileSync(CONTRACT_ABI_PATH, "utf8"));
+const contract = new ContractPromise(api, abiJSON, CONTRACT_ADDRESS);
+const gasLimit = api.registry.createType('WeightV2', {
+          refTime: 300000000000,
+          proofSize: 500000,
+});
+const storageDepositLimit = null;
+
+/// Reading and processing the CSV file
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const stream = fs.createReadStream("./vested_address2.csv").pipe(csv());
+
+stream.on("data", async (data) => {
+  stream.pause(); // pause stream while processing
+
+  try {
+    console.log("No:", data.No);
+    console.log("Address:", data.Address);
+    console.log("Balance:", data.Balance);
+
+    // perform the operation here (add vested balance)
+    const keyring = new Keyring({ type: "sr25519" });
+    const alice = keyring.addFromUri(ALICE);
+    await new Promise(async (resolve, reject) => {
+      const unsub = await contract.tx
+        .addVestedBalance({ storageDepositLimit, gasLimit }, 
+          String(data.Address ?? "").trim(),
+          String(data.Balance).replace(/"/g, "").trim(),
+        ).signAndSend(alice, ({ status, events, dispatchError }) => {    
+          console.log("Status:", status?.type);
+          if(events?.length > 0) {
+            events.forEach(({ event }) => {
+              if (event.section === "contracts" && event.method === "ContractEmitted") {
+                console.log(decode(event.data));
+                unsub();
+                resolve();
+              }
+            });
+          }
+      });
+    });
+
+    // 30 seconds delay
+    await sleep(5000); 
+
+  } catch (err) {
+    console.error("Error processing row:", err);
+
+  } finally {
+    stream.resume(); // resume stream
+
+  }
+  
+});
+
+stream.on("end", () => {
+  console.log("CSV processing completed.");
+});
